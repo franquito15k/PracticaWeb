14b8bf004bce66f4a2e0c52e37a782c2a4091e14
franquitopapa <gabrielfrangomez146@gmail.com>
2023-10-13 18:12:51 -0300

    Fin de proyecto ("ponele q proyecto xd")

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
 const $botonSumar = document.querySelector("#botonesS");
 const $botonRestar = document.querySelector("#botonesR");
 const $display = document.querySelector("#display");
+const $reset = document.querySelector("#reset");
+
 let cont = 0;
+$display.textContent = "Incrementa el numero: " + cont;
 
 $botonSumar.addEventListener("click", function () {
     cont++;
@@ -12,3 +15,8 @@ $botonRestar.addEventListener("click", function () {
     cont--;
     $display.textContent = "Incrementa el numero: " + cont;
 });
+
+$reset.addEventListener("click",function() {
+    cont=0;
+    $display.textContent = "Incrementa el numero: " + cont;
+})
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  darkmode: false,
+  variants:{
+    extends:{}
+  },
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
